120c4bdb1a5ea8218702db1ccd1ad239183c0e13
joaopedropastura <jpsoares111@hotmail.com>
2023-10-20 14:59:33 -0300

    Quase Lá

--- JavaScript/engine.js
+++ JavaScript/engine.js
@@ -1,10 +1,52 @@
-comando  = [0, 1, 3, 2, 4, -1, 2, 5, 6]
+
+let stack = []
+
+const sum = () => {
+    let result = 0
+    stack.forEach(elem => {
+        result = result + parseInt(elem)
+    });
+    console.log(result) 
+}
+const commands = {
+    "DFVR436TFDG" : console.log,
+    "DGSF546D4GS" : sum
+}
 
 
-printa = 1 = printf
-console.log('sla')
+const fs = require('fs');
+const input = []
+const data = fs.readFileSync('../output.omnitrix', 'utf8');
 
-if( comando[i] == "DFVR436TFDG")
-{
-    console.log( comando[i++])
+const dataSplited = data.split(',')
+const dataFinal = dataSplited.map(x => x.replaceAll("'", "").replaceAll("[", "").replaceAll("]", '').replaceAll(" ", ''))
+
+// dataFinal.forEach(elem => {
+//     if(elem == "DFVR436TFDG")
+        
+// });
+
+let flag = false
+let command = ''
+for (i = 0; i < dataFinal.length; i++){
+    if(dataFinal[i] == "SKEJ4345DFG"){
+        flag = false
+        commands[command](stack.toString().replaceAll(',,', ' ').replaceAll(',', ''))
+        stack = []
+        continue
+    }
+
+    if(dataFinal[i] == "DFVR436TFDG"){
+        command = dataFinal[i]
+        flag = true
+        continue
+    }
+
+    if(dataFinal[i] == "DGSF546D4GS"){
+        command = dataFinal[i]
+        flag = true
+        continue
+    }
+    if(flag)
+        stack.push(dataFinal[i])
 }
--- Python/main.py
+++ Python/main.py
@@ -1,16 +1,68 @@
-commands = { "printa": "DFVR436TFDG",  "soma" : "DGSF546D4GS", "if": "25GF27478SD", "else": "ASDF42452FV", "loop": "OIHE432ERFV" }
+commands = { "AlienX": "DFVR436TFDG",  "4Bracos" : "DGSF546D4GS", "FogoSelvagem": "25GF27478SD", "Chama": "ASDF42452FV", "EcoEco": "OIHE432ERFV", "end": "SKEJ4345DFG", "jump": "SEOHOREI453" }
+output = []
 
-f = open("sla.minhalinguagem", "r")
-
-
-["DFVR436TFDG" , 3, 3, '+', 5, 'R', "DGSF546D4GS"]
+f = open("../main.omnitrix", "r")
 
 file = f.readlines()
+
 for line in file:
-    print(line)
+    if line[0] == '\n':
+        continue
+    command = line.split('(')[0]
+    if command in commands.keys():
+        output.append(commands[command])
+
+    
+    values = line.split('(')[1].split(')')[0]
+
+    if output[-1] == "DFVR436TFDG":
+        for i in values:
+            output.append(i)
+        output.append(commands["end"])
+    if output[-1] == "DGSF546D4GS":
+        values = values.split(',')
+        for i in values:
+            output.append(i)
+        output.append(commands["end"])
+
+f.close()
 
 
-output = []
+fileOutput = open("../output.omnitrix", "w")
+fileOutput.write(str(output))
+fileOutput.close()
+
+
+
+
+
+
+
+# for line in file:
+#     if line[0] == '\n':
+#         continue
+#     command = line.split('(')[0]
+#     if command in commands.keys():
+#         output.append(commands[command])
+
+#     values = line.split('(')[1].split(')')[0]
+    
+#     string = []
+#     flag = False
+#     for l in values:
+#         if l == "'" or l == '"':
+#             flag = not flag
+#             print(flag)
+#         if flag:
+#             string.append(l)
+#     m = str(string).replace("'", '')
+        
+        
+
+    
+    
+# print(output)
+
 # for i, key in commands.items():
 #     print("chave: " , key)
 #     print("valor: ",  i)
