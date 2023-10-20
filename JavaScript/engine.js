37730ac184e8560bbf32e03ae9d979a82ac34b52
joaopedropastura <jpsoares111@hotmail.com>
2023-10-20 09:56:27 -0300

    init proj

---
+++ JavaScript/engine.js
@@ -0,0 +1,10 @@
+comando  = [0, 1, 3, 2, 4, -1, 2, 5, 6]
+
+
+printa = 1 = printf
+console.log('sla')
+
+if( comando[i] == "DFVR436TFDG")
+{
+    console.log( comando[i++])
+}
---
+++ Python/main.py
@@ -0,0 +1,16 @@
+commands = { "printa": "DFVR436TFDG",  "soma" : "DGSF546D4GS", "if": "25GF27478SD", "else": "ASDF42452FV", "loop": "OIHE432ERFV" }
+
+f = open("sla.minhalinguagem", "r")
+
+
+["DFVR436TFDG" , 3, 3, '+', 5, 'R', "DGSF546D4GS"]
+
+file = f.readlines()
+for line in file:
+    print(line)
+
+
+output = []
+# for i, key in commands.items():
+#     print("chave: " , key)
+#     print("valor: ",  i)
